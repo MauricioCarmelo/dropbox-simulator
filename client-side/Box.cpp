557125fc7fd5589b4ba02fd759b871d21439a738
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-15 19:07:03 -0300

    tests

--- client-side/Box.cpp
+++ client-side/Box.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
+
     std::cout << "Hello, I am the client side" << std::endl;
     return 0;
 }
--- server-side/Warehouse.cpp
+++ server-side/Warehouse.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
+
     std::cout << "Hello, I am the server side" << std::endl;
     return 0;
 }
+
