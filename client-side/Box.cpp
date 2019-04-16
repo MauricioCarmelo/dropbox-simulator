e307b33c191ac765bb1013114922d0c2d0fde11f
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-16 17:15:39 -0300

    Receiving the data from the terminal (client side)

--- client-side/Box.cpp
+++ client-side/Box.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <cstring>
+#include "Client.h"
+
+#define USERNAME_SIZE 25
+
+using namespace std;
+
+char username[USERNAME_SIZE];
+
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "Hello, I am the client side" << std::endl;
+    if (argc < 4) {
+        std::cout << "Please use" << std::endl;
+        std:: cout << "./box <user> <address> <port>" << std::endl;
+        exit(1);
+    }
+
+    strcpy(username, argv[1]);
+    char *host = (char*)malloc(strlen(argv[2]));
+    strcpy(host, argv[2]);
+    int port = atoi(argv[3]);
+
+    cout << host << " " << port << " " << username << endl;
+
+    //Client client(host, 4000);
+    //client.establishConnectionToHost();
+
     return 0;
 }
