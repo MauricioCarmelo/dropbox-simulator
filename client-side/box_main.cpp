4b869a09390b3e99f8d3149fe86a9f79c2fb26eb
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-17 23:16:19 -0300

    changed main function location.

---
+++ client-side/box_main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include "Client.h"
+#include "Box.h"
+
+#define USERNAME_SIZE 25
+
+using namespace std;
+
+char username[USERNAME_SIZE];
+
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 4) {
+        std::cout << "Please use" << std::endl;
+        std:: cout << "./box <user> <address> <port>" << std::endl;
+        exit(1);
+    }
+
+    strcpy(username, argv[1]);
+    char *host = (char*)malloc(strlen(argv[2]));
+    strcpy(host, argv[2]);
+    int port = atoi(argv[3]);
+
+    Box box;
+    box.open(host, port);
+
+    return 0;
+}
