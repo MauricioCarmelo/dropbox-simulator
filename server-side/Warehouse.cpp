cba8b295025b836a62c28b81c9340ba73cc3997d
Gustavo <tavinho_correa@hotmail.com>
2019-04-20 19:14:27 -0300

    Code for the server side

--- server-side/Warehouse.cpp
+++ server-side/Warehouse.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <sys/socket.h>
+#include "Server.h"
 
 int main() {
 
-    std::cout << "Hello, I am the server side" << std::endl;
+    int serverSocket, newSocket;
+    struct sockaddr_storage serverWarehouse;
+    socklen_t addressSize;
+
+    Server newServer;
+
+    serverSocket = newServer.createServerSocket("127.0.0.1", 4200);
+
+    while (true){
+
+        std::cout << "Waiting for client..." << std::endl;
+
+        listen(serverSocket,10);
+
+        addressSize = sizeof serverWarehouse;
+        newSocket = accept(serverSocket, (struct sockaddr *) &serverWarehouse, &addressSize);
+
+       // newServer.send_file(testFile,newSocket); TODO send file to server
+    }
+
     return 0;
 }
 
