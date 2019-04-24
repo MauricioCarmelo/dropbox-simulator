141f0b34bc714c0e06791620f19ffb5b3352292d
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-23 21:31:20 -0300

    server receving array of char in a loop

--- server-side/Server.cpp
+++ server-side/Server.cpp
@@ -5,11 +5,27 @@
 #include "Server.h"
 #include <cstring>
 
-int Server::createServerSocket(std::string host, int port) {
+Server::Server() = default;
 
-    int serverSocket;
-    struct sockaddr_in serverAddress;
+int Server::createSocket(char* host, int port) {
 
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        printf("ERROR opening socket");
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(serv_addr.sin_zero), 8);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        printf("ERROR on binding");
+
+    listen(sockfd, 5);
+
+    clilen = sizeof(struct sockaddr_in);
+
+    /*if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        printf("ERROR opening socket");
     //First parameter is the internet domain IPV4
     //Second parameter means we are using TCP
     //Third paramer tells the OP to use the default protocol
@@ -18,28 +34,65 @@ int Server::createServerSocket(std::string host, int port) {
     serverAddress.sin_port = htons(port);
     serverAddress.sin_addr.s_addr = INADDR_ANY; //TODO use host parameter here instead
     memset(serverAddress.sin_zero, '\0', sizeof serverAddress.sin_zero);
+    if (bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0)
+        printf("ERROR on binding");
 
-    bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)); // Connects socket with server address
+    listen(serverSocket, 5);
 
-    return serverSocket;
+    clilen = sizeof(struct sockaddr_in);*/
+
+    //return serverSocket;
+    return 0;
 }
 
+int Server::receive_file() {
+
+    char message[10];
+    int file_size = 7;
+    int bytes_received;
+    char buffer[BUFFER_SIZE];
+
+    if ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) == -1)
+        printf("ERROR on accept");
+
+    bytes_received = 0;
+    while(bytes_received < file_size) {
+        bzero(buffer, BUFFER_SIZE);
+
+        /* read from the socket */
+        n = read(newsockfd, buffer, BUFFER_SIZE);
+        if (n < 0)
+            printf("ERROR reading from socket");
+
+        memcpy(message+bytes_received, buffer, n);
+        bytes_received = bytes_received + n;
+        std::cout << "Bytes received: " << bytes_received << std::endl;
+    }
+
+    n = write(newsockfd, "Hi", 3);
+    if (n < 0)
+        printf("ERROR writing to socket");
+
+    for (char c: message)
+        std::cout << c;
+    std::cout << std::endl;
+
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
+
+
+/*
 void Server::sync_server() {
 
 }
 
-void Server::receive_file(File file, int socket) {
-
-}
 
 void Server::send_file(File file, int socket) {
 
 }
-
-Server::Server() = default;
-
-Server::Server(const File &fileInfo, const Client &client) : file_info(fileInfo), client(client) {}
-
+*/
 
 
 
--- server-side/Server.h
+++ server-side/Server.h
@@ -5,40 +5,45 @@
 #ifndef DROPBOX_SERVER_H
 #define DROPBOX_SERVER_H
 
-
 #include "../client-side/include/File.h"
 #include "../client-side/include/Client.h"
+
+#define BUFFER_SIZE 4
 
 class Server {
 
 private:
 
-    File file_info;
+    // We will need arrays of sockets here in order to handle multiple users
+
+    int sockfd, newsockfd, n;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    /*int serverSocket;
+    struct sockaddr_in serverAddress;
+    socklen_t clilen;
+    struct sockaddr_in cli_addr;
+    //File file_info;*/
     Client client;
 
 public:
 
     Server();
-
-    Server(const File &fileInfo, const Client &client);
-
+    ~Server() {};
+    //Server(const File &fileInfo, const Client &client);
+    int createSocket(char* host, int port);
+    int receive_file();
+/*
     void sync_server();                 //TODO Syncs server with directory "sync_dir_<nomeusuário>" and client
 
-    void receive_file(File file, int socket);      // TODO Receives a file from client. Should be executed when receiving a file file - path/filename.ext
-
-    int createServerSocket(std::string host, int port);
-
     void send_file(File file, int socket);
-
     const File &getFileInfo() const;
-
     void setFileInfo(const File &fileInfo);
-
     const Client &getClient() const;
-
     void setClient(const Client &client);
     // TODO Sends file to client - filename.ext
+    */
 };
-
 
 #endif //DROPBOX_SERVER_H
