35afe7b36e2c30a957611fb77f5e4f8aeae920fe
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-25 00:13:41 -0300

    inserting warehouse_main.cpp file

---
+++ server-side/warehouse_main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sys/socket.h>
+#include "Server.h"
+#include "Warehouse.h"
+
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        std::cout << "Please use" << std::endl;
+        std:: cout << "./warehouse <port>" << std::endl;
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+
+    Warehouse warehouse;
+    warehouse.run(port);
+
+    return 0;
+}
