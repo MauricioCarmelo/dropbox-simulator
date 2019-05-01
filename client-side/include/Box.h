76d514746d00ed06c3558c9c5b77419d9b70dfda
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-05-01 18:20:01 -0300

    inserted interface() method

--- client-side/include/Box.h
+++ client-side/include/Box.h
@@ -11,18 +11,35 @@
 
 #define SYNC_DIR "sync_dir"
 
+#define UPLOAD 2
+#define DOWNLOAD 3
+#define DELETE 4
+#define LIST_SERVER 5
+#define LIST_CLIENT 6
+#define GET_SYNC_DIR 7
+#define EXIT 8
+#define INVALID_COMMAND -1
+
+struct Instruction {
+    char command_name[50];
+    int command_id;
+    char path[50];
+    char filename[50];
+};
+
 class Box {
 
 private:
     Client client;
     SystemDAO sda;
-
 public:
     Box();
     ~Box() {};
     int open(char *host, int port);
     bool createSyncDir();
-
+    void interface();
+    Instruction prepare_instruction(char line[]);
+    int get_command_id(char *command);
 };
 
 #endif //DROPBOX_BOX_H
